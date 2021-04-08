2fb21c5fecff79edda8c7383632bbdcf817c57f7
lify <lifengyong@gmail.com>
2021-04-08 13:50:27 +0800

    修改文件

--- springboot-javaconsole/src/main/java/com/neu/lify/console/SpringBootConsoleApplication.java
+++ springboot-javaconsole/src/main/java/com/neu/lify/console/SpringBootConsoleApplication.java
@@ -1,6 +1,7 @@
 package com.neu.lify.console;
 
 import com.neu.lify.console.apple.RedApple;
+import com.neu.lify.console.orange.BigOrange;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -11,6 +12,8 @@ public class SpringBootConsoleApplication implements CommandLineRunner {
 
     @Autowired
     private RedApple apple;
+    @Autowired
+    private BigOrange orange;
 
     public static void main(String[] args) throws Exception {
 
@@ -23,5 +26,7 @@ public class SpringBootConsoleApplication implements CommandLineRunner {
         String s = "spring boot start";
         System.out.println(s);
         System.out.println(apple.getAppleColor());
+
+        System.out.println(orange.getOrangeColor());
     }
 }
