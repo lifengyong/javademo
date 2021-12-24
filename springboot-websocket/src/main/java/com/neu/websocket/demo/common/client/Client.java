df033da69ac11beee0c82bdb6ccd86947fa114ca
lify <lifengyong@gmail.com>
2021-12-24 17:36:24 +0800

    add client

---
+++ springboot-websocket/src/main/java/com/neu/websocket/demo/common/client/Client.java
@@ -0,0 +1,61 @@
+package com.neu.websocket.demo.common.client;
+
+import org.java_websocket.client.WebSocketClient;
+import org.java_websocket.drafts.Draft;
+import org.java_websocket.handshake.ServerHandshake;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.ByteBuffer;
+import java.util.Map;
+
+public class Client extends WebSocketClient {
+
+  public Client(URI serverUri, Draft draft) {
+    super(serverUri, draft);
+  }
+
+  public Client(URI serverURI) {
+    super(serverURI);
+  }
+
+  public Client(URI serverUri, Map<String, String> httpHeaders) {
+    super(serverUri, httpHeaders);
+  }
+
+  @Override
+  public void onOpen(ServerHandshake handshakedata) {
+    send("{\"msg\":\"formclient\"}");
+    System.out.println("opened connection");
+  }
+
+  @Override
+  public void onMessage(String message) {
+    System.out.println("received: " + message);
+  }
+
+  @Override
+  public void onMessage(ByteBuffer byteBuffer) {
+    System.out.println("received: bytebuffer" );
+  }
+
+  @Override
+  public void onClose(int code, String reason, boolean remote) {
+    System.out.println(
+        "Connection closed by " + (remote ? "remote peer" : "us") + " Code: " + code + " Reason: "
+            + reason);
+  }
+
+  @Override
+  public void onError(Exception ex) {
+    ex.printStackTrace();
+  }
+
+  public static void main(String[] args) throws URISyntaxException {
+    Client c = new Client(new URI("ws://localhost:10001/ws-demo/websocket/abc"));
+    c.connect();
+  }
+
+}
